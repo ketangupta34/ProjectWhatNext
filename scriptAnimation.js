d920c77bb844aa2f21e91c7f9331518a3be77c25
Ketan Gupta <ketangupta34@gmail.com>
2020-04-17 22:50:56 +0530

    Discribtion animation

--- scriptAnimation.js
+++ scriptAnimation.js
@@ -30,19 +30,50 @@ new ScrollMagic.Scene({
 .addIndicators()
 .addTo(controller);
 
+var logoPath = {
+    values:[
+        {x:320,y:0}
+]};
+var tween2 = new TweenLite.to('#disLogo',1,{
+    bezier:logoPath
+});
+new ScrollMagic.Scene({
+    triggerElement:".discription",
+    triggerHook:0.55,
+    duration:100
+})
+.setTween(tween2)
+.addIndicators()
+.addTo(controller);
 
+
+var disPath = {
+    values:[
+        {x:'-110%',y:0}
+]};
+var tween3 = new TweenLite.to('#disInfo',1,{
+    bezier:disPath
+});
+new ScrollMagic.Scene({
+    triggerElement:".discription",
+    triggerHook:0.55,
+    duration:100
+})
+.setTween(tween3)
+.addIndicators()
+.addTo(controller);
 // ScrollReveal
 // ScrollReveal.debug = true;
-ScrollReveal().reveal('#disLogo',{
-    reset: true,
-    duration: 500,
-    delay: 300,
-    origin: 'left',
-    distance: '100px'
-});
-ScrollReveal().reveal('#disInfo',{
-    // reset:true,
-    delay: 300,
-    origin: 'right',
-    distance: '100px'
-});
+// ScrollReveal().reveal('#disLogo',{
+//     reset: true,
+//     duration: 500,
+//     delay: 300,
+//     origin: 'left',
+//     distance: '100px'
+// });
+// ScrollReveal().reveal('#disInfo',{
+//     // reset:true,
+//     delay: 300,
+//     origin: 'right',
+//     distance: '100px'
+// });
